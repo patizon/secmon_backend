16fdab4ba009d76a0e8efbd3e6d37f4606ff824c
Milan Poliak <milan.poliak@pixwell.sk>
2016-10-20 02:26:32 +0200

    test, v tomto branchi sa hrajte. NEMERGOVAT

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=secmon',
+    'username' => 'secmon',
+    'password' => 'secmon',
+    'charset' => 'utf8',
+];
